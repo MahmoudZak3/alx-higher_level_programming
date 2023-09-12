fe4669bb1131b7282bc5823544cb46e07c1b3b91
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-09-12 22:36:09 +0300

    zak com

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -3,14 +3,14 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * print_listint - listint_t list to be printed
+ * @h:  head of list pointer
+ * Return: nodes
  */
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
-    unsigned int n; /* number of nodes */
+    unsigned int n;
 
     current = h;
     n = 0;
@@ -25,10 +25,10 @@ size_t print_listint(const listint_t *h)
 }
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * add_nodeint_end -  new node to add to the end of a listint_t
+ * @head: pointer  first node of listint_t pointer
+ * @n: include integer in new node
+ * Return: the new element address  or NULL
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
@@ -57,9 +57,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 }
 
 /**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
+ * free_listint - free  listint_t
+ * @head: pointer  list to  free
+ * Return: 0
  */
 void free_listint(listint_t *head)
 {
