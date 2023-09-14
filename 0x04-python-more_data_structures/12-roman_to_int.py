fb884cf7e237944890800dc36483faba28a764aa
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-09-14 18:33:41 +0300

    zak com

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,13 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+    if roman_string and isinstance(roman_string, str):
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r_s = roman_string
+        numero = 0
+        for i in range(len(r_s)):
+            if i > 0 and (rom[r_s[i]] > rom[r_s[i - 1]]):
+                numero += rom[r_s[i]] - (2 * rom[roman_string[i - 1]])
+            else:
+                numero += rom[r_s[i]]
+        return numero
+    return 0
