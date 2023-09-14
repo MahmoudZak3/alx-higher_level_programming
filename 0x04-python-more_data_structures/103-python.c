3aa43b53b67fe7b7e93edbeb840dcf03bd84dae6
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-09-14 18:08:54 +0300

    zak com

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,8 +4,8 @@ void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
 /**
- * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject list object.
+ * print_python_list -  info about Python lists printing.
+ * @p:  PyObject  object.
  */
 void print_python_list(PyObject *p)
 {
@@ -31,8 +31,8 @@ void print_python_list(PyObject *p)
 }
 
 /**
- * print_python_bytes - Prints basic info about Python byte objects.
- * @p: A PyObject byte object.
+ * print_python_list -  info about Python lists printing.
+ * @p:  PyObject  object.
  */
 void print_python_bytes(PyObject *p)
 {
